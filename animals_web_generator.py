379ef14eecdb92bf961ff9da6af1033cc53c417e
hasmikhovhannisyan <hasmik.hovhannisyan86@gmail.com>
2026-02-06 10:17:47 +0100

    Teil 2

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,8 +1,25 @@
 import json
+
 def load_data(file_path):
-  """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
+    """ Loads a JSON file """
+    with open(file_path, "r", encoding="utf-8") as handle:
+        return json.load(handle)
+
+
+def iterates_data(data):
+    output = ""
+
+    for animal_data in data:
+        output += f'Name: {animal_data.get("name", "Unknown")}\n'
+
+        output += f'Diet: {animal_data.get("characteristics", {}).get("diet", "Unknown")}\n'
+
+        output += f'Location: {animal_data.get("locations", ["Unknown"])[0]}\n'
+
+        output += f'Type: {animal_data.get("characteristics", {}).get("type", "Unknown")}\n\n'
+
+    print(output)
+
 
 animals_data = load_data('animals_data.json')
-print (animals_data)
+iterates_data(animals_data)
